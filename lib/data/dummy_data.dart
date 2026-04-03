42f0cd8c612817f52d1fb20b0cd21f1dd72ed361
subexi <h_dittrich@me.com>
2026-04-03 17:36:26 +0200

    165 Introducing the Stack Widget

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -68,7 +68,7 @@ const dummyMeals = [
     affordability: Affordability.affordable,
     complexity: Complexity.simple,
     imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+        'https://cdn.pixabay.com/photo/2018/07/18/19/12/spaghetti-3547078_1280.jpg',
     duration: 20,
     ingredients: [
       '4 Tomatoes',
